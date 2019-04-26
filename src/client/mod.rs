837ff93bf48512871097610bd2beeea3b8b6149b
Ravi Shankar <wafflespeanut@gmail.com>
2019-04-26 14:21:11 +0530

    Add feature gate for Spaces stuff

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -1,6 +1,8 @@
-mod builder;
+#[cfg(feature = "spaces")] mod builder;
 /// Later we can make a different client and implement it as a feature.
 mod reqwest;
+#[cfg(feature = "spaces")] mod spaces;
 
+#[cfg(feature = "spaces")]
 pub(crate) use self::builder::RequestBuilder;
 pub use self::reqwest::Client;
